e1f0fa421083967fdf80462327fe76b8989080b8
Emre Kul <emre.kul@trendyol.com>
2019-02-19 22:12:55 +0300

    aaaaa

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const app = express();
 
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+
+const adapter = new FileSync('db.json')
+const db = low(adapter)
+
+db.defaults({ data: []}).write()
+
 app.listen(process.env.PORT | 3333, () => {
     
     app.get('/', function (req, res) {
         res.send('hello world')
-      })
+    })
+    app.get('/set', function (req, res) {
+        db.get('data').push({header: req.headers}).write();
+        res.send('ok').status(200);
+    })
+    app.get('/get', function (req, res) {
+        res.send(db.get('data').value()).status(200);
+    })
+    
 
 });
